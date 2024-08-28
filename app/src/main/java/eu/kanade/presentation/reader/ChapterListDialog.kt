07928a5986430fdcab47014bdd47e6f931351cd7
NGB-Was-Taken <76197326+NGB-Was-Taken@users.noreply.github.com>
2024-08-28 17:19:33 +0000

    idk

--- app/src/main/java/eu/kanade/presentation/reader/ChapterListDialog.kt
+++ app/src/main/java/eu/kanade/presentation/reader/ChapterListDialog.kt
@@ -45,7 +45,7 @@ fun ChapterListDialog(
     val context = LocalContext.current
     val state = rememberLazyListState(chapters.indexOfFirst { it.isCurrent }.coerceAtLeast(0))
     val downloadManager: DownloadManager = remember { Injekt.get() }
-    val progressFlow by downloadManager.progressFlow().collectAsState(emptyList<Download>())
+    val progressList by downloadManager.progressFlow().collectAsState(emptyList<Download>())
 
     AdaptiveSheet(
         onDismissRequest = onDismissRequest,
@@ -59,7 +59,7 @@ fun ChapterListDialog(
                 items = chapters,
                 key = { "chapter-${it.chapter.id}" },
             ) { chapterItem ->
-                val activeDownload = progressFlow.find { it.chapter.id == chapterItem.chapter.id }
+                val activeDownload = progressList.find { it.chapter.id == chapterItem.chapter.id }
                 val downloaded = if (chapterItem.manga.isLocal()) {
                     true
                 } else {
