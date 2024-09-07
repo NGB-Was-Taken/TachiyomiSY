cfb84c31a60bad9069729e2b069d8c05dfe13738
NGB-Was-Taken <76197326+NGB-Was-Taken@users.noreply.github.com>
2024-09-07 16:53:14 +0000

    Barebones setup \(Only AniList works\).

--- app/src/main/java/eu/kanade/tachiyomi/data/track/anilist/AnilistApi.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/anilist/AnilistApi.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.data.track.anilist
 import android.net.Uri
 import androidx.core.net.toUri
 import eu.kanade.tachiyomi.data.database.models.Track
+import eu.kanade.tachiyomi.data.track.model.TrackMangaMetadata
 import eu.kanade.tachiyomi.data.track.model.TrackSearch
 import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.network.awaitSuccess
@@ -302,6 +303,76 @@ class AnilistApi(val client: OkHttpClient, interceptor: AnilistInterceptor) {
                     }
             }
         }
+    }
+
+    suspend fun getMangaMetadata(track: DomainTrack): TrackMangaMetadata {
+        return withIOContext {
+            val query = """
+            |query (${'$'}mangaId: Int!) {
+                |Media (id: ${'$'}mangaId) {
+                    |id
+                    |title {
+                        |userPreferred
+                    |}
+                    |coverImage {
+                        |extraLarge
+                    |}
+                    |description
+                    |staff {
+                        |edges {
+                            |role
+                            |node {
+                                |name {
+                                    |userPreferred
+                                |}
+                            |}
+                        |}
+                    |}
+                |}
+            |}
+            |
+            """.trimMargin()
+            val payload = buildJsonObject {
+                put("mangaId", track.remoteId)
+            }
+            with(json) {
+                authClient.newCall(
+                    POST(
+                        API_URL,
+                        body = payload.toString().toRequestBody(jsonMime),
+                    ),
+                )
+                    .awaitSuccess()
+                    .parseAs<JsonObject>()
+                    .let {
+                        val data = it["data"]!!.jsonObject
+                        val media = data["Media"]!!.jsonObject
+                        jsonToTrackMangaMetadata(media)
+                    }
+            }
+        }
+    }
+
+    private fun jsonToTrackMangaMetadata(struct: JsonObject): TrackMangaMetadata {
+        val edges = struct["staff"]!!.jsonObject["edges"]!!.jsonArray
+        val authors = edges.filter {
+            it.jsonObject["role"]!!.jsonPrimitive.content == "Story"
+        }.map {
+            it.jsonObject["node"]!!.jsonObject["name"]!!.jsonObject["userPreferred"]!!.jsonPrimitive.content
+        }.joinToString(", ")
+        val artists = edges.filter {
+            it.jsonObject["role"]!!.jsonPrimitive.content == "Art"
+        }.map {
+            it.jsonObject["node"]!!.jsonObject["name"]!!.jsonObject["userPreferred"]!!.jsonPrimitive.content
+        }.joinToString(", ")
+        return TrackMangaMetadata(
+            struct["id"]!!.jsonPrimitive.long,
+            struct["title"]!!.jsonObject["userPreferred"]!!.jsonPrimitive.content,
+            struct["coverImage"]!!.jsonObject["extraLarge"]!!.jsonPrimitive.contentOrNull,
+            struct["description"]!!.jsonPrimitive.contentOrNull,
+            authors,
+            artists,
+        )
     }
 
     private fun jsonToALManga(struct: JsonObject): ALManga {
